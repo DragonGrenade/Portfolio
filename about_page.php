97c116b76a19a48bbfa37f4d7297d70e516aecf3
Chris K. <72310639+DragonGrenade@users.noreply.github.com>
2026-03-24 10:01:20 +0200

    Update about_page.php

--- about_page.php
+++ about_page.php
@@ -6,32 +6,32 @@
     <title>Document</title>
 
 
-    <link rel="stylesheet" href="css/portfolio_pieces_styles.css">
-    <link rel="stylesheet" href="css\contact_styles.css">
-    <link rel="stylesheet" href="css\back_button_styles.css">
-    <link rel="stylesheet" href="css\footer.css">
+    <link rel="stylesheet" href="https://chriswebdesign.wasmer.app/css/portfolio_pieces_styles.css">
+    <link rel="stylesheet" href="https://chriswebdesign.wasmer.app/css\contact_styles.css">
+    <link rel="stylesheet" href="https://chriswebdesign.wasmer.app/css\back_button_styles.css">
+    <link rel="stylesheet" href="https://chriswebdesign.wasmer.app/css\footer.css">
 
-<link rel="stylesheet" href="css\styles.css">
-
-
-
-    <link rel="stylesheet" href="css\about_section_styles.css">
+<link rel="stylesheet" href="https://chriswebdesign.wasmer.app/css\styles.css">
 
 
 
-    <script src="javascript/email_script.js" async></script>
+    <link rel="stylesheet" href="https://chriswebdesign.wasmer.app/css\about_section_styles.css">
+
+
+
+    <script src="https://chriswebdesign.wasmer.app/javascript/email_script.js" async></script>
 </head>
 <body>
 
-    <?php include("includes/about_first_include.php"); ?>
+    <?php include("https://chriswebdesign.wasmer.app/includes/about_first_include.php"); ?>
 
 
 
 
-<?php include("includes/contact_include.php"); ?>
+<?php include("https://chriswebdesign.wasmer.app/includes/contact_include.php"); ?>
 
 
-<?php include("includes/footer.php"); ?>
+<?php include("https://chriswebdesign.wasmer.app/includes/footer.php"); ?>
 
 </body>
 </html>
